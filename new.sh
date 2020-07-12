60e3694fe70551919c90720a06b73de5e9731647
phani-rudra9 <53288879+phani-rudra9@users.noreply.github.com>
2020-07-12 23:46:26 +0530

    Create new.sh

---
+++ new.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+TASK_FAMILY="ttn-qa-webapp-task"
+SERVICE_NAME="ttn-qa-webapp-service"
+NEW_DOCKER_IMAGE="phani9-devops:22"
+CLUSTER_NAME= "ttn-qa-cluster"
+OLD_TASK_DEF=$(aws ecs describe-task-definition --task-definition $TASK_FAMILY --output json)
+NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $NEW_DOCKER_IMAGE '.taskDefinition.containerDefinitions[0].image=$NDI')
+FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
+aws ecs register-task-definition --family $TASK_FAMILY --cli-input-json "$(echo $FINAL_TASK)"
+aws ecs update-service --service $SERVICE_NAME --task-definition $TASK_FAMILY --cluster $CLUSTER_NAME
