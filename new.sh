ce25062919719a9bae82756e7c952002ad69c202
phani-rudra9 <53288879+phani-rudra9@users.noreply.github.com>
2020-07-13 23:11:35 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -3,10 +3,10 @@ AWS_REGION="ap-south-1"
 TASK_FAMILY="ttn-qa-webapp-task"
 SERVICE_NAME="ttn-qa-webapp-task"
 NEW_DOCKER_IMAGE="nginx"
-CLUSTER_NAME= "sample"
+CLUSTER_NAME= "ttn-qa-cluster"
 OLD_TASK_DEF=$(aws ecs describe-task-definition --task-definition $TASK_FAMILY --output json)
 NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $NEW_DOCKER_IMAGE '.taskDefinition.containerDefinitions[0].image=$NDI')
 FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
 aws ecs register-task-definition --family $TASK_FAMILY --cli-input-json "$(echo $FINAL_TASK)"
-aws ecs update-service --service ${SERVICE_NAME} --cluster sample --task-definition ${TASK_FAMILY} --desired-count 3 --region ${AWS_REGION}
+aws ecs update-service --service ${SERVICE_NAME} --cluster ttn-qa-cluster --task-definition ${TASK_FAMILY} --desired-count 3 --region ${AWS_REGION}
 
