6a166987853e331c909f595a7ae1cfb39a5e8e43
phani-rudra9 <53288879+phani-rudra9@users.noreply.github.com>
2020-07-14 22:56:28 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,7 +12,7 @@ aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE --task-definit
 
 DECRIBED_SERVICE=$(aws ecs describe-services --region $AWS_REGION --cluster $CLUSTER_NAME --services $SERVICE);
 CURRENT_DESIRED_COUNT=$(echo $DECRIBED_SERVICE | jq --raw-output ".services[0].desiredCount")
-    #    - echo $CURRENT_DESIRED_COUNT
+echo $CURRENT_DESIRED_COUNT
 
 CURRENT_TASK_REVISION=$(echo $DECRIBED_SERVICE | jq -r ".services[0].taskDefinition")
 echo "Current Task definition in Service" + $CURRENT_TASK_REVISION
@@ -22,13 +22,13 @@ echo $CURRENT_RUNNING_TASK
 
 CURRENT_STALE_TASK=$(echo $DECRIBED_SERVICE | jq -r ".services[0].deployments | .[] | select(.taskDefinition != \"$CURRENT_TASK_REVISION\") | .taskDefinition")
 echo "Task defn apart from current service Taskdefn" +  $CURRENT_STALE_TASK
-    #   - echo $CURRENT_STALE_TASK
+echo $CURRENT_STALE_TASK
 
 tasks=$(aws ecs --region $AWS_REGION list-tasks --cluster $CLUSTER_NAME | jq -r '.taskArns | map(.[40:]) | reduce .[] as $item (""; . + $item + " ")')
 echo "Tasks are as follows" 
 echo $tasks
 TASKS=$(aws ecs --region $AWS_REGION describe-tasks --cluster $CLUSTER_NAME --task $tasks);
-    #    - echo $TASKS
+echo $TASKS
 OLDER_TASK=$(echo $TASKS | jq -r ".tasks[] | select(.taskDefinitionArn!= \"$CURRENT_TASK_REVISION\") | .taskArn | split(\"/\") | .[1] ")
 echo "Older Task running  " + $OLDER_TASK
 for old_task in $OLDER_TASK; do
