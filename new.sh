ed14c048307df82d089b271087489e3bf8e9e285
phani-rudra9 <53288879+phani-rudra9@users.noreply.github.com>
2020-07-14 19:40:18 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -8,5 +8,5 @@ OLD_TASK_DEF=$(aws ecs describe-task-definition --task-definition $TASK_FAMILY -
 NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $NEW_DOCKER_IMAGE '.taskDefinition.containerDefinitions[0].image=$NDI')
 FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
 aws ecs register-task-definition --family $TASK_FAMILY --cli-input-json "$(echo $FINAL_TASK)"
-aws ecs update-service --service ${SERVICE_NAME} --cluster phani --task-definition ${TASK_FAMILY} --desired-count 3 --region ${AWS_REGION}
+aws ecs update-service --service ${SERVICE_NAME} --cluster phani --task-definition ${TASK_FAMILY} --desired-count 3 --region ${AWS_REGION} --launch-type fargate
 
