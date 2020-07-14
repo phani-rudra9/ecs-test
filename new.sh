ae27d6edfd0309553c0c3b2eb37972ba98346e8c
phani-rudra9 <53288879+phani-rudra9@users.noreply.github.com>
2020-07-14 20:15:53 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 AWS_REGION="ap-south-1"
-TASK_FAMILY="ttn-qa-webapp-task"
-SERVICE_NAME="ttn-qa-webapp-task"
+TASK_FAMILY="fargate-task-definition"
+SERVICE_NAME="test"
 NEW_DOCKER_IMAGE="nginx"
 CLUSTER_NAME= "phani"
 OLD_TASK_DEF=$(aws ecs describe-task-definition --task-definition $TASK_FAMILY --output json)
 NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $NEW_DOCKER_IMAGE '.taskDefinition.containerDefinitions[0].image=$NDI')
 FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
 aws ecs register-task-definition --family $TASK_FAMILY --cli-input-json "$(echo $FINAL_TASK)"
-aws ecs update-service --service ${SERVICE_NAME} --cluster phani --task-definition ${TASK_FAMILY} --desired-count 3 --region ${AWS_REGION} --launch-type fargate
+aws ecs update-service --service ${SERVICE_NAME} --cluster phani --task-definition ${TASK_FAMILY} --desired-count 3 --region ${AWS_REGION} --launch-type FARGATE
 
